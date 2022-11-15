7206735a6dae7e036e5b51d153c540aabc01327b
kendimuthomi <kendijackline42@gmail.com>
2022-11-15 10:48:15 +0000

    Check if leaf

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,10 +7,7 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node == NULL)
+	if (node == NULL || node->left != NULL || node->left != NULL)
 		return (0);
-	if (node->left != NULL || node->left != NULL)
-		return (0);
-	else
-		return (1);
+	return (1);
 }
