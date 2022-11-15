e0a25b9525ea99433afdf0db8f3f28515a4b146e
kendimuthomi <kendijackline42@gmail.com>
2022-11-15 18:55:48 +0000

    Check if a binary tree is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -21,7 +21,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
  *   * @tree: pointer to the root node of the
  *    *  tree to measure the height
  *     *  Return: size of tree
- *      */
+ */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t l_height = 0;
@@ -41,7 +41,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
  *   * @tree: pointer to the root node of the
  *    *  tree to measure the balance factor
  *     * Return: balance factor
- *      */
+ */
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	size_t left_height = 0;
