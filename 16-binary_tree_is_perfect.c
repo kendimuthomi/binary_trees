a21d93bc1846a6654a7fee305991a9500b698447
kendimuthomi <kendijackline42@gmail.com>
2022-11-16 09:19:52 +0000

    Binary tree is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -7,14 +7,17 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
+	int left = 0, right = 0;
 	if (!tree)
 		return (0);
 	if (!tree->left && !tree->right)
 		return (1);
 	if (tree->left && tree->right)
 	{
-		binary_tree_is_full(tree->left);
-		binary_tree_is_full(tree->right);
+		left = binary_tree_is_full(tree->left);
+		right = binary_tree_is_full(tree->right);
+		if (left == 0 || right == 0)
+			return (0);
 		return (1);
 	}
 	return (0);
