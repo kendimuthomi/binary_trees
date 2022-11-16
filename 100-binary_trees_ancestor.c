c55c7fd37a05e10e30b146d866b38b186c7dc485
kendimuthomi <kendijackline42@gmail.com>
2022-11-16 08:04:45 +0000

    Least Common Ancestor

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -15,7 +15,7 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 	if (first == NULL || second == NULL)
 		return (NULL);
 	if (first == second)
-		return ((binary_tree_t*)first);
+		return ((binary_tree_t *)first);
 	l = first->parent;
 	r = second->parent;
 
