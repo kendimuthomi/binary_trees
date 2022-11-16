383fdee7a88da585dc35e63d9594433aad2ac96f
kendimuthomi <kendijackline42@gmail.com>
2022-11-16 10:07:07 +0000

    level-order traversal

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -21,3 +21,37 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (r_height);
 }
 
+/**
+ * print_level - prints the nodes by level
+ * @func: pointer to a function
+ * @level: the level
+ * @node: pointer to the node
+ */
+void print_level(binary_tree_t *node, void (*func)(int), int level)
+{
+	if (node == NULL || func == NULL)
+		return;
+	if (level == 1)
+	{
+		print_level(node->left, func, level - 1);
+		print_level(node->right, func, level - 1);
+	}
+}
+
+/**
+ * binary_tree_levelorder - goes through a binary
+ * tree using level-order traversal
+ * @tree: pointer to the root node of the tree to traverse
+ * @func: pointer to a function to call for each node
+ */
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+	size_t h = 0, i;
+	binary_tree_t *tree2 = (binary_tree_t *)tree;
+
+	if (tree == NULL || func == NULL)
+		return;
+	h = binary_tree_height(tree);
+	for (i = 0; i <= h; i++)
+		print_level(tree2, func, i);
+}
--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -63,7 +63,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 /**
  * binary_tree_is_perfect - checks if a binary tree is perfect
  * @tree: pointer to the root node of the tree to check
- * Return: 0 if perfect 1 if not
+ * Return: 1 if perfect 0 if not
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
