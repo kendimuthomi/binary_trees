174190522e3e87bca8ad622f5f8138f5abcf05c0
kendimuthomi <kendijackline42@gmail.com>
2022-11-16 09:13:19 +0000

    Binary tree is perfect

---
+++ 101-binary_tree_levelorder.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the
+ *  tree to measure the height
+ *  Return: size of tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t l_height = 0;
+	size_t r_height = 0;
+
+	if (tree == NULL)
+		return (0);
+	l_height += 1 + binary_tree_height(tree->left);
+	r_height += 1 + binary_tree_height(tree->right);
+	if (l_height > r_height)
+		return (l_height);
+	else
+		return (r_height);
+}
+
--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -12,8 +12,11 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (!tree->left && !tree->right)
 		return (1);
 	if (tree->left && tree->right)
-		return (binary_tree_is_full(tree->left) &&
-				binary_tree_is_full(tree->right));
+	{
+		binary_tree_is_full(tree->left);
+		binary_tree_is_full(tree->right);
+		return (1);
+	}
 	return (0);
 }
 /**
